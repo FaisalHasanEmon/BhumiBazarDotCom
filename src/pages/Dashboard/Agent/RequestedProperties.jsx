3344c6c251a3e801badc041d76b6717885f4b274
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-01-29 21:27:02 +0600

    Agent Dashboard Requested propertye accept and reject section complete

--- src/pages/Dashboard/Agent/RequestedProperties.jsx
+++ src/pages/Dashboard/Agent/RequestedProperties.jsx
@@ -2,20 +2,47 @@ import React from "react";
 import useTransactions from "../../../hooks/useTransactions";
 import DashboardPageHeading from "../../../components/Shared/DashboardPageHeading/DashboardPageHeading";
 import userUserInfo from "../../../hooks/userUserInfo";
-
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useTheme from "../../../hooks/useTheme";
 const RequestedProperties = () => {
+  const axiosSecure = useAxiosSecure();
+  const { notifySuccess } = useTheme();
   const [transactions, isTransactionsLoading, refetchTransactions] =
     useTransactions();
+  // Get current user info
   const [userInfo, isUserPending] = userUserInfo();
+
   if (isTransactionsLoading) {
     return <div>Loading...</div>;
   }
+
+  // Get my transaction information
   const myTransactions = transactions?.filter(
     (item) => item?.agentEmail === userInfo.email
   );
+  console.log(myTransactions);
 
-  const handleAccept = () => {};
-  const handleReject = () => {};
+  // Handle Accept to pay
+  const handleAccept = async (transactionId, propertyId) => {
+    console.log(transactionId, propertyId);
+    const res = await axiosSecure.put("/transaction-accept", {
+      transactionId,
+      propertyId,
+    });
+    console.log(res.data);
+    if (res.data.modifiedCount > 0) {
+      notifySuccess("Offer Accepted");
+      refetchTransactions();
+    }
+  };
+  const handleReject = async (id) => {
+    console.log(id);
+    const res = await axiosSecure.patch("/transaction-reject", { id });
+    if (res.data.modifiedCount > 0) {
+      notifySuccess("Offer Rejected");
+      refetchTransactions();
+    }
+  };
   return (
     <div>
       <DashboardPageHeading
@@ -29,6 +56,10 @@ const RequestedProperties = () => {
             <tr>
               <th className="text-xl text-black font-semibold">Agent Info</th>
               <th className="text-xl text-black font-semibold">Buyer's Info</th>
+              <th className="text-xl text-black font-semibold">
+                Offered Price
+              </th>
+              <th className="text-xl text-black font-semibold">Deadline</th>
               <th className="text-xl text-black font-semibold">Status</th>
             </tr>
           </thead>
@@ -36,6 +67,7 @@ const RequestedProperties = () => {
             {/* row 1 */}
             {myTransactions.map((property) => (
               <tr key={property?._id}>
+                {/* Agent Info */}
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -54,6 +86,7 @@ const RequestedProperties = () => {
                     </div>
                   </div>
                 </td>
+                {/* Buyer's Info */}
                 <td>
                   {property?.userName}
                   <br />
@@ -61,6 +94,21 @@ const RequestedProperties = () => {
                     {property?.userEmail}
                   </span>
                 </td>
+                {/* Offered Price */}
+                <td>
+                  {property?.offeredPrice}TK
+                  <br />
+                  <span className="badge badge-ghost badge-sm bg-orange-500 text-white font-bold">
+                    Asking:({property?.priceRange.minPrice}-
+                    {property?.priceRange.maxPrice})TK
+                  </span>
+                </td>
+                {/* Deadline */}
+                <td>
+                  {property?.deadline}
+                  <br />
+                </td>
+                {/* Status */}
                 <td>
                   {(property?.agentValidation).toLowerCase() === "pending" && (
                     <>
@@ -72,11 +120,31 @@ const RequestedProperties = () => {
                           Reject
                         </button>
                         <button
-                          onClick={() => handleAccept(property?._id)}
+                          onClick={() =>
+                            handleAccept(property?._id, property?.propertyId)
+                          }
                           className="btn btn-sm bg-green-500 hover:bg-green-500 text-white hover:text-white font-bold hover:font-bold hover:border-2 hover:border-gray-500 "
                         >
                           Accept
                         </button>
+                      </div>
+                    </>
+                  )}
+                  {(property?.agentValidation).toLowerCase() === "rejected" && (
+                    <>
+                      <div className="flex">
+                        <p className=" bg-red-500  text-white font-bold p-1 rounded-lg ">
+                          Rejected
+                        </p>
+                      </div>
+                    </>
+                  )}
+                  {(property?.agentValidation).toLowerCase() === "accepted" && (
+                    <>
+                      <div className="flex">
+                        <p className=" bg-green-500  text-white font-bold p-1 rounded-lg ">
+                          Accepted
+                        </p>
                       </div>
                     </>
                   )}
