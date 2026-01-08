ed190fb78e7e35cc1da302196a6cf38e32fcc072
FaisalHasanEmon <faisalhasan5833@gmail.com>
2026-01-09 01:11:07 +0600

    user review component updated

--- src/components/HomePageComponents/UserReviews.jsx
+++ src/components/HomePageComponents/UserReviews.jsx
@@ -7,15 +7,15 @@ const UserReviews = () => {
   if (isReviewsLoading) {
     return <Loading></Loading>;
   }
-  const advertisingReview1 = userReviews?.filter(
-    (review) => review?._id === "679a206b3a63cf888e91ecdd"
-  );
-  const advertisingReview2 = userReviews?.filter(
-    (review) => review?._id === "679a21c73a63cf888e91ecde"
-  );
-  const advertisingReview3 = userReviews?.filter(
-    (review) => review?._id === "679a2262377fa232b4d8349f"
-  );
+  const advertisingReview1 = (
+    Array.isArray(userReviews) ? userReviews : []
+  )?.filter((review) => review?._id === "679a206b3a63cf888e91ecdd");
+  const advertisingReview2 = (
+    Array.isArray(userReviews) ? userReviews : []
+  )?.filter((review) => review?._id === "679a21c73a63cf888e91ecde");
+  const advertisingReview3 = (
+    Array.isArray(userReviews) ? userReviews : []
+  )?.filter((review) => review?._id === "679a2262377fa232b4d8349f");
   const adReview = [
     ...advertisingReview1,
     ...advertisingReview2,
@@ -23,22 +23,36 @@ const UserReviews = () => {
   ];
 
   return (
-    <div className="flex flex-col md:flex-row gap-2 justify-center items-center ">
-      {adReview?.map((review) => (
-        <div key={review?._id} className="relative">
-          <figure className="w-[300px] h-[300px] overflow-clip border rounded-lg">
-            <img
-              src={review?.reviewerPhoto}
-              className="object-cover h-full w-full"
-            ></img>
-          </figure>
-          <div className="rounded-lg backdrop-blur-md bg-white/30 h-[100px] w-1/2 md:w-[200px] absolute bottom-5 md:translate-x-12 translate-x-1/2 p-2 flex flex-col justify-center items-center gap-1 ">
-            <Rating style={{ maxWidth: 100 }} value={review.rating} readOnly />
-            <p className="text-xs text-gray font-semibold">{review?.review}</p>
-          </div>
+    <>
+      {adReview ? (
+        <div className="flex flex-col md:flex-row gap-2 justify-center items-center ">
+          {adReview?.map((review) => (
+            <div key={review?._id} className="relative">
+              <figure className="w-[300px] h-[300px] overflow-clip border rounded-lg">
+                <img
+                  src={review?.reviewerPhoto}
+                  className="object-cover h-full w-full"
+                ></img>
+              </figure>
+              <div className="rounded-lg backdrop-blur-md bg-white/30 h-[100px] w-1/2 md:w-[200px] absolute bottom-5 md:translate-x-12 translate-x-1/2 p-2 flex flex-col justify-center items-center gap-1 ">
+                <Rating
+                  style={{ maxWidth: 100 }}
+                  value={review.rating}
+                  readOnly
+                />
+                <p className="text-xs text-gray font-semibold">
+                  {review?.review}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <div>
+          <p className="">Nothing Found</p>
+        </div>
+      )}
+    </>
   );
 };
 
